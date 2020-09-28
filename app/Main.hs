ecd3da940b4da9fb09044c4c3f424e007abb811c
Dima Kurilo <dkurilo@gmail.com>
2020-09-28 17:25:21 -0400

    slightly improved message

--- app/Main.hs
+++ app/Main.hs
@@ -6,6 +6,6 @@ import           System.IO
 main :: IO ()
 main = do
     hSetBuffering stdin NoBuffering
-    putStrLn "How many games you want to play?"
+    putStrLn "How many games you want to play? (even number)"
     n <- read <$> getLine
     play n
